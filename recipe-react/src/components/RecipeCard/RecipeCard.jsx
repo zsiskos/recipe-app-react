e478ca93b738343b720b14acc9bbc48fc884b1a6
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-03 17:18:25 -0400

    create static components

--- recipe-react/src/components/RecipeCard/RecipeCard.jsx
+++ recipe-react/src/components/RecipeCard/RecipeCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RecipeCard = props => {
     return (
         <div className='RecipeCard'>
-            The recipes
+            Recipe Card
         </div>
     );
 }
--- recipe-react/src/pages/App/App.js
+++ recipe-react/src/pages/App/App.js
@@ -1,17 +1,14 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from '../../components/Header/Header'
 import RecipeBox from '../../components/RecipeBox/RecipeBox'
-import NavBar from '../../components/NavBar/NavBar'
+
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          My Recipe Box
-        </header>
-        <NavBar />
+        <Header />
         <RecipeBox />
       </div>
     );
