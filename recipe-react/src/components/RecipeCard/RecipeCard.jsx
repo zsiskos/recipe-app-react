7461371cf05b2a7d3e4fe5f1f40955751f21523d
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-05 09:32:45 -0400

    add bootstrap cards to recipe card component

--- recipe-react/src/components/RecipeCard/RecipeCard.jsx
+++ recipe-react/src/components/RecipeCard/RecipeCard.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 const RecipeCard = props => {
     return (
         <div className='RecipeCard'>
-            Recipe Card
+           <div 
+                className="card" 
+                style={{width: '18em'}}
+            >
+                <img 
+                    src="https://i.imgur.com/iPRrXaM.jpg" 
+                    class="card-img-top" 
+                    alt="..."
+                />
+                <div class="card-body">
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
