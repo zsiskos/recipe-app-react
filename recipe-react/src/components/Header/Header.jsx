48b8c4051337eef4f49049686bf2f262b6d0fade
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-05 15:49:09 -0400

    add style to profile page

--- recipe-react/src/components/Header/Header.jsx
+++ recipe-react/src/components/Header/Header.jsx
@@ -9,29 +9,55 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
 
 const Header = (props) => {
-    return (
-        <Container>
-            <Row>
-                <Col className='col-small' sm={4}>
-                <Image src="https://i.imgur.com/YUceMfV.png" fluid />
-                    <Button variant="info">
-                        <Link
-                            to='/signup'
-                            className='NavBar-link'
-                        >
-                            Join
-                        </Link>
-                    </Button>
-                </Col>
-                <Col className='col-bigger' sm={8}><Image src="https://i.imgur.com/iPRrXaM.jpg" fluid /></Col>
-            </Row>
+    let header = props.user ?
+    <Container className='Header'>
+        <Row>
+            <Col className='col-small-user' sm={4}>
+            <Image 
+                src="https://quiviracoalition.org/wp-content/uploads/2019/02/generic-person-icon-300x300.png" 
+                roundedCircle
+                style={{
+                    height: 171,
+                    width: 180,
+                    border: '1px solid grey',
+                  }}
+            />
+            </Col>
+            <Col className='col-bigger' sm={8}>
+                <p>This is where the person's profile info will go</p>
+            </Col>
+        </Row>
             <NavBar 
                 user={props.user}
                 handleLogout={props.handleLogout}
             />
-        </Container>
+    </Container>
+    :
+    <Container>
+        <Row>
+            <Col className='col-small' sm={4}>
+            <Image src="https://i.imgur.com/YUceMfV.png" fluid />
+                <Button variant="info">
+                    <Link
+                        to='/signup'
+                        className='NavBar-link'
+                    >
+                        Join
+                    </Link>
+                </Button>
+            </Col>
+            <Col className='col-bigger' sm={8}><Image src="https://i.imgur.com/iPRrXaM.jpg" fluid /></Col>
+        </Row>
+            <NavBar 
+                user={props.user}
+                handleLogout={props.handleLogout}
+            />
+    </Container>
+    return (
+        <div className='NavBar'>
+            {header}
+        </div>
     )
 }
-
 
 export default Header;
